0ce3f3d48343171a4cdff3282b9122584f4d6be0
Nicolas Viveros <nviverosb@neiatec.com>
2020-05-11 08:05:29 -0500

    New args format and function name verification

--- lib/rut/helpers/helpers.ex
+++
@@ -1,5 +0,0 @@
-defmodule Rut.Helpers do
-  def remove_spaces(string) do
-    
-  end
-end
--- lib/rut/rut.ex
+++ lib/rut/rut.ex
@@ -1,34 +1,30 @@
 defmodule Rut do
 
   alias Rut.Extract
-  alias Rut.Extract.Natural
-  alias Rut.Extract.Legal
 
-  def get_info(route) do
+  def get(route, fields \\ []) do
     if File.exists?(route) do
       new_file_name = parse_name(route)
       stream = File.stream!(route)
-      IO.inspect(new_file_name, label: "NEW FILE NAME")
-      System.cmd("mutool", ["draw", "-o", "#{new_file_name}.html", "-q", "-F", "html", route]) # -q is for quite mode
-      System.cmd("mutool", ["draw", "-o", "#{new_file_name}.txt", "-q", "-F", "txt", route]) # -q is for quite mode
+      System.cmd("mutool", ["draw", "-o", "#{new_file_name}.html", "-q", "-F", "html", route]) # -q is for quiet mode (no logs)
       case File.read(new_file_name <> ".html") do
           {:ok, hmtl} -> 
             {:ok, document} = Floki.parse_document(hmtl)
             document = Floki.find(document, "#page1")
-            info = get_general_info(document)
-            File.rm!(route)
+            info = get_info(document, fields)
+            File.rm!(new_file_name <> ".html")
             info
-          {:error, :enoent} -> raise "File does not exist"
+          {:error, :enoent} -> raise "Rut: File does not exist"
           error ->
               IO.inspect(error, label: "Error") 
-              raise "File conversion unsuccesful"
+              raise "Rut: File conversion unsuccesful"
       end
     else
-      raise "File does not exist! Make sure you give a valid route."
+      raise "Rut: File does not exist! Make sure you give a valid route."
     end
   end
 
-  def get_general_info(document) do
+  def get_info(document, fields) do
     {width, height, page1} = Extract.page(document, 1)
     page1 = Extract.delete_images(page1)
     # From this index we're going to start to fetch the rest of data
@@ -43,6 +39,7 @@ defmodule Rut do
         y*width + x
       end)
     
+    # Use this for debugging purposes (finding where is each field)
     # titles
     #   |> Enum.slice(25..60)
     #   |> IO.inspect(label: "Titles")
@@ -53,26 +50,35 @@ defmodule Rut do
         {x, y} = get_xy_pos(element)
         y*width + x
       end)
+
+    {_,_,_, extract_data} = Enum.reduce(fields, {start_index, titles, values, %{}}, fn x, acc -> 
+      if Keyword.has_key?(Extract.__info__(:functions), x) do
+        apply(Extract, x, [acc])
+      else
+        IO.puts "Rut: Invalid attribute: #{x}... skipping"
+        acc
+      end
+    end)
     
-    {_,_,_, extract_data} = {start_index, titles, values, %{}}
-      |> Extract.nit()
-      |> Extract.dv
-      |> Extract.person_type
-      |> Extract.person_type_code
-      |> Extract.business_name
-      |> Extract.country
-      |> Extract.country_code
-      |> Extract.state
-      |> Extract.state_code
-      |> Extract.city
-      |> Extract.city_code
-      |> Extract.address
-      |> Extract.email
-      |> Extract.phone_number_1
-      |> Extract.phone_number_2
-      |> Extract.main_activity
-      |> Extract.secondary_activity
-      |> Extract.responsibilities
+    # {_,_,_, extract_data} = {start_index, titles, values, %{}}
+    #   |> Extract.nit()
+    #   |> Extract.dv
+    #   |> Extract.person_type
+    #   |> Extract.person_type_code
+    #   |> Extract.business_name
+    #   |> Extract.country
+    #   |> Extract.country_code
+    #   |> Extract.state
+    #   |> Extract.state_code
+    #   |> Extract.city
+    #   |> Extract.city_code
+    #   |> Extract.address
+    #   |> Extract.email
+    #   |> Extract.phone_number_1
+    #   |> Extract.phone_number_2
+    #   |> Extract.main_activity
+    #   |> Extract.secondary_activity
+    #   |> Extract.responsibilities
 
     extract_data
   end
